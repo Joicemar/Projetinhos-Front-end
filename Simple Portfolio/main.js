4b60f057db416adefccb7a925c497b3ea402afa4
Joicemar <33008360+Joicemar@users.noreply.github.com>
2023-04-23 15:30:54 -0300

    Add files via upload

--- Simple Portfolio/main.js
+++ Simple Portfolio/main.js
@@ -1,17 +1,17 @@
 let typingText = new Typed("#text", {
-  strings: ["Joicemar", "Programador", "Dev front-end"],
-  loop: true,
-  backDelay: 3000,
-  typeSpeed: 100,
-  backSpeed: 30,
-  loop: true,
+    strings: ["Meu nome é Joicemar", "Sou Dev front-end", "Sou Programador"],
+    loop: true,
+    backDelay: 3000,
+    typeSpeed: 80,
+    backSpeed: 20,
+    loop: true,
 })
 
 const circle = document.querySelector(".dayNight");
 let body = document.querySelector(".body");
 
 
-let dayNight = document.querySelector("#box");
+let dayNight = document.querySelector("#day-select");
 dayNight.addEventListener('click', function () {
     if (body.classList.contains("day")) {
         body.classList.remove("day");
@@ -30,3 +30,23 @@ bars.addEventListener('click', function () {
     menu.classList.toggle('active');
 
 });
+
+// Cursor do mouse:
+const mouse_circle = document.querySelector('.circle');
+
+document.addEventListener('mouseleave', () => {
+    const minhaDiv = document.getElementById('circle');
+    minhaDiv.style.display = 'none';
+});
+document.addEventListener('mouseenter', () => {
+    const minhaDiv = document.getElementById('circle');
+    minhaDiv.style.display = 'block';
+});
+
+document.addEventListener("mousemove", function (event) {
+    let x = event.clientX - mouse_circle.offsetWidth / 2;
+    let y = event.clientY - mouse_circle.offsetHeight / 2;
+    mouse_circle.style.left = x + "px";
+    mouse_circle.style.top = y + "px";
+});
+
